9f4b56634c0ec877ef723089e0321caf903418d8
DevCeur <dev.carlos.umana@gmail.com>
2021-01-07 14:19:24 -0500

    feat: add basic page transitions

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -1,4 +1,11 @@
 import Head from "next/head";
+import { motion } from "framer-motion";
+
+const pageWrapperVariants = {
+  hidden: { opacity: 0, translateX: -5 },
+  visible: { opacity: 1, translateX: 0 },
+  exited: { opacity: 0, translateX: 5 },
+};
 
 const PageWrapper = ({ title, children }) => {
   return (
@@ -6,7 +13,15 @@ const PageWrapper = ({ title, children }) => {
       <Head>
         <title>My Website - {title}</title>
       </Head>
-      <div>{children}</div>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        exit="exited"
+        variants={pageWrapperVariants}
+        transition={{ duration: 0.2 }}
+      >
+        {children}
+      </motion.div>
     </>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { AnimatePresence } from "framer-motion";
 
 import Navigation from "../components/Navigation";
 
 import styles from "../styles/App.module.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <>
       <Navigation />
-      <div className={styles.appContainer}>
-        <Component {...pageProps} />
-      </div>
+      <AnimatePresence exitBeforeEnter>
+        <div className={styles.appContainer} key={router.route}>
+          <Component {...pageProps} />
+        </div>
+      </AnimatePresence>
     </>
   );
 }
